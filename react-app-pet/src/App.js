55327eb78ca85ecc37aa8d336c653addd8516c29
FelipeRoveroni <rm95430@fiap.com.br>
2022-10-01 14:38:26 -0300

    App.js atualizado e ListaFormularios criada

---
+++ react-app-pet/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css'
+import ListaFormularios from "./Components/ListaFormularios";
+
+function App(){
+    return(
+        <>
+        <ListaFormularios/>
+        </>
+    );
+}
+
+export default App;
